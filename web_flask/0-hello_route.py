977f5a5c316d25cae3af77d4537f8479a01bc944
andres reyes <andressantiagore@gmail.com>
2020-09-02 11:37:29 -0500

    add style to the folder

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-     """ an simply routing """
-     return 'Hello HBNB!'
+    """ an simply routing """
+    return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
-     app.run()
+    app.run()
